7d3d7a477781de447ca3af85ea72ae8718de53f8
non.mu <mnexpo5@yahoo.co.jp>
2019-06-22 01:23:31 +0000

    task_login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,16 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :correct_user, only: [:show]
+  
   def index
    @users = User.order(id: :desc).page(params[:page]).per(25)
   end
 
   def show
    @user = User.find(params[:id])
+  @tasks = @user.tasks.order(id: :desc).page(params[:page])
+  counts(@user)
+   
   end
 
   def new
@@ -21,11 +27,18 @@ class UsersController < ApplicationController
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
     end
-  end
+  end  
+  
+  private
   
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
   
-  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 end
